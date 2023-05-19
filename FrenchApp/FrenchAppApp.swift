b8436bbd19deda47b8db8c129c53a72722f80f2f
andrearamonroy <andrearamonroy@gmail.com>
2023-05-19 16:13:00 -0500

    Initial Commit

---
+++ FrenchApp/FrenchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchAppApp.swift
+//  FrenchApp
+//
+//  Created by Andrea on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct FrenchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
