13d220e533e383296eadba4bbc7c7dc67db401fa
andrearamonroy <andrearamonroy@gmail.com>
2023-06-18 14:23:58 -0500

    s3 upload and download data

--- FrenchApp/ContentView.swift
+++ FrenchApp/ContentView.swift
@@ -60,12 +60,71 @@ struct DialogView: View {
                     }
                 }
             }
+            PodcastView2()
         }
     }
 }
 
+struct PodcastView: View {
+    var body: some View {
+        VStack {
+            Text("Hello")
+            Button("Upload Data") {
+                Task {
+                    do {
+                        try await uploadData()
+                    } catch {
+                        print("Error: \(error)")
+                    }
+                }
+            }
+        }
+    }
+    
+    func uploadData() async throws {
+        let dataString = "Example file contents"
+        let data = Data(dataString.utf8)
+        let uploadTask = Amplify.Storage.uploadData(
+            key: "ExampleKey2",
+            data: data
+        )
+        for await progress in await uploadTask.progress {
+            print("Progress: \(progress)")
+        }
+        let value = try await uploadTask.value
+        print("Completed: \(value)")
+    }
+}
 
-
+struct PodcastView2: View {
+    var body: some View {
+        VStack {
+            Text("Hello")
+            Button("Download Data") {
+                Task {
+                    await downloadData()
+                }
+            }
+        }
+    }
+    
+    func downloadData() async {
+        do {
+            let downloadTask = Amplify.Storage.downloadData(key: "ExampleKey2")
+            
+            for await progress in await downloadTask.progress {
+                print("Progress: \(progress)")
+            }
+            
+            let data = try await downloadTask.value
+            print("Completed: \(data)")
+            
+            // Add your logic to handle the downloaded data here
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+}
 
 
 
