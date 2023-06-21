eec2f5865545ec8a8111067d4bb65310536bbdfd
andrearamonroy <andrearamonroy@gmail.com>
2023-06-21 10:02:24 -0500

    added predictions pluggin

--- FrenchApp/FrenchAppApp.swift
+++ FrenchApp/FrenchAppApp.swift
@@ -13,14 +13,14 @@ import AWSAPIPlugin
 import AWSCognitoAuthPlugin
 import AWSS3StoragePlugin
 import Authenticator
+import AWSPredictionsPlugin
 
 
 @main
 struct FrenchAppApp: App {
-    //private let dataService = ProductionDataService()
-  //  private let audioPlayer = AudioPlayer()
+   
     
-    init(){
+   init(){
         configureAmplify()
     }
     
@@ -38,7 +38,6 @@ struct FrenchAppApp: App {
                 }
                 
             }
-            //ContentView(dataService: ProductionDataService())
         }
     }
 }
@@ -47,16 +46,18 @@ func configureAmplify() {
     let dataStorePlugin = AWSDataStorePlugin(modelRegistration: AmplifyModels())
     let s3 = AWSS3StoragePlugin()
     let cognitoAuth = AWSCognitoAuthPlugin()
+    let predictions = AWSPredictionsPlugin()
     
     do {
         try Amplify.add(plugin: apiPlugin)
         try Amplify.add(plugin: dataStorePlugin)
         try Amplify.add(plugin: s3)
         try Amplify.add(plugin: cognitoAuth)
+        try Amplify.add(plugin: predictions)
         try Amplify.configure()
         print("Initialized Amplify");
     } catch {
-        // simplified error handling for the tutorial
+        // simplified error handling
         print("Could not initialize Amplify: \(error)")
     }
 }
